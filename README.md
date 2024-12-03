05595a749b1c9b8b096b2c5ffc85052d8765cf82
Filippo Di Marco <filidm5795@gmail.com>
2024-12-03 23:59:18 +0100

    feat(main.rs): create and implement day2 part 1 exercise

---
+++ README.md
@@ -0,0 +1,25 @@
+# red-nosed-report
+
+## Requisiti  
+
+- **Rust**
+
+## Installation  
+
+1. Clone the repository:  
+   ```bash
+   git clone https://github.com/tuo-utente/red-nosed-report.git
+   cd red-nosed-report
+   ```  
+
+2. Complie the project:  
+   ```bash
+   cargo build --release
+   ```  
+
+## Usage  
+
+Exec the program with  
+```bash
+./target/release/red-nosed-report
+```  
---
+++ src/main.rs
@@ -0,0 +1,50 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    
+    let path = "assets/example_lists.txt";
+    let mut safe = 0;
+
+    let file = File::open(path);
+    let reader = io::BufReader::new(file);
+
+    let mut matrix: Vec<Vec<i32>> = Vec::new();
+
+    for line in reader.lines() {
+        let line = line?;
+
+        let row: Vec<i32> = line
+            .split_whitespace()
+            .filter_map(|s| s.parse::<i32>().ok())
+            .collect();
+
+        matrix.push(row); // Aggiunge la riga alla matrice
+    }
+
+    for row in &matrix {
+        let diff: Vec<i32> = row.windows(2)
+            .map(|pair| pair[0] - pair[1])
+            .collect();
+
+        let abs_diff_sum: i32 = diff.windows(2)
+            .map(|pair| pair[0].abs() + pair[1].abs())
+            .sum();
+
+        let diff_sum: i32 = diff.windows(2)
+            .map(|pair| pair[0] + pair[1])
+            .sum::<i32>(); // Specifica il tipo di ritorno come i32
+
+        if abs_diff_sum == diff_sum {
+            let correct2 = diff.iter().all(|&num| num.abs() <= 3);
+
+            if correct2 {
+                safe += 1;
+            }
+        }
+
+        println!("{:?}", diff);
+    }
+    println!("{:?}", safe);
+    OK(())
+}
